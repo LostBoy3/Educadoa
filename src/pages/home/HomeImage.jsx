74cc57467bc8db8bddc3f5ad9c8b29e27ad4419e
M26nofx <marcel.hfr1@gmail.com>
2026-05-25 20:24:22 -0300

    feat: improve scroll animation with framer motion

--- src/pages/home/HomeImage.jsx
+++ src/pages/home/HomeImage.jsx
@@ -1,39 +1,26 @@
-import { useEffect, useState } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 import ImgKid from "../../assets/kidimg.svg";
 
 function HomeImage() {
-  const [scrollY, setScrollY] = useState(0);
+  const { scrollY } = useScroll();
 
-  useEffect(() => {
-    function handleScroll() {
-      setScrollY(window.scrollY);
-    }
+  const scale = useTransform(scrollY, [0, 1000], [1, 1.15]);
 
-    window.addEventListener("scroll", handleScroll);
+  const y = useTransform(scrollY, [0, 1000], [0, 80]);
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  const scale = Math.min(1.2, 1 + scrollY * 0.0005);
-
-  const pt = Math.min(80, 10 + scrollY * 0.2);
-
-  const rotate = Math.min(scrollY * 0.002, 2);
+  const rotate = useTransform(scrollY, [0, 1000], [0, 2]);
 
   return (
-    <div className="flex justify-center pt-10 pb-50 w-full fade-up">
-      <img
+    <div className="hidden lg:flex justify-center pt-10 pb-50 w-full">
+      <motion.img
         src={ImgKid}
         alt="kid"
-        className="w-full max-w-[850px] rounded-[50px] object-cover shadow-2xl transition-transform duration-300 will-change-transform"
         style={{
-          transform: `
-          scale(${scale})
-          translateY(${pt}px)
-          rotate(${rotate}deg)`,
+          scale,
+          y,
+          rotate,
         }}
+        className="w-full max-w-[850px] rounded-[50px] object-cover shadow-2xl will-change-transform"
       />
     </div>
   );
